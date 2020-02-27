1888f57125aa96d484bd25a8d85392ab4ec4d343
KevinYang <kevin.yang@wisecore.com.tw>
2020-02-27 19:16:49 +0800

    add some example script

---
+++ deepo_base/run_generate.sh
@@ -0,0 +1,2 @@
+python generate.py --cuda-ver 10.2 --cudnn-ver 7 Dockerfile.deepo_base opencv==3.4.1 pytorch tensorflow==1.14 caffe
+
---
+++ deepo_dev/run_generate_dev.sh
@@ -0,0 +1,2 @@
+python generate.py --cuda-ver 10.2 --cudnn-ver 7 Dockerfile.deepo_dev pytorch tensorflow caffe
+
